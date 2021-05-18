d6c08533cebcb8db8a781493b172326c016662b8
ps3vdo <79896188191@ya.ru>
2021-05-18 21:02:17 +0300

    add notesRoutes.js, folder db, /index.js, db/notes.js

---
+++ routes/notesRoutes.js
@@ -0,0 +1,11 @@
+const Router = require('express');
+const notesController = require('../controller/notesController');
+
+const router = Router();
+
+router.get('/notes', notesController.getAllOwnerNotes);
+router.post('/notes', notesController.createNote);
+router.put('/notes', notesController.updateNote);
+router.delete('/notes/:id', notesController.updateNote);
+
+module.exports = router
