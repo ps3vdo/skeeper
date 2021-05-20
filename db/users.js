0cfe395a07b6efbcd0b4764e82fb990294fb2e4d
ps3vdo <79896188191@ya.ru>
2021-05-20 21:27:25 +0300

    add endpoint userUpdate

--- db/users.js
+++ db/users.js
@@ -2,12 +2,15 @@ const db = require('./db');
 const apiError = require('../error/apiError')
 
 class UsersServices {
+
     currentDate = new Date();
+
     async create (email, hash_password, salt, first_name, last_name) {
         console.log(email, hash_password, salt, first_name, last_name)
         return (await db.query('INSERT INTO users (email, hash_password, salt, first_name, last_name, created_at, updated_at) values ($1, $2, $3, $4, $5, $6, $6) RETURNING id, email',
             [email, hash_password, salt, first_name, last_name, this.currentDate])).rows[0];
     }
+
     async getUser(id) {
         try {
             return await db.query('SELECT * FROM users where id = $1', [id]);
@@ -15,6 +18,7 @@ class UsersServices {
             return apiError.badRequest(e.message);
         }
     }
+
     async getUsers(limit, offset) {
         try {
             return await db.query('SELECT * FROM users LIMIT $1 OFFSET $2', [limit, offset]);
@@ -22,14 +26,18 @@ class UsersServices {
             return apiError.badRequest(e.message);
         }
     }
+
     async validate (email) {
         return await (db.query('SELECT FROM users where email = $1', [email]));
     }
-    async createRefreshToken(id, token) {
+
+    async createRefreshToken(id, token) {//TODO запись токена в бд
     }
+
     async update(first_name, last_name, email, id) {
-        return await db.query('UPDATE owner set first_name = $1, last_name = $2 email = $3, updated_at = $4 where id = $5 RETURNING *',
-            [first_name, last_name, email, id, this.currentDate]);
+        console.log(email)
+        return await db.query('UPDATE users set first_name = $1, last_name = $2, email = $3, updated_at = $4 where id = $5 RETURNING *',
+            [first_name, last_name, email,  this.currentDate, id]);
 
     }
 }
