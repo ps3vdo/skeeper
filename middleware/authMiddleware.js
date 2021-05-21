a62e99ca63cf9cead01df1a99134aec84a4af368
Vladimir <59231371+ps3vdo@users.noreply.github.com>
2021-05-21 11:36:24 +0300

    Add files via upload

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,19 @@
+const ApiError = request('../error/apiError');
+
+function authorization(req, res, next) {
+	try{
+	const {tokenAccess} = req.headers.authorization;
+	
+	if (!tokenAccess) res(ApiError.forbidden("Not authorization"));
+
+	const user = verifyAccessToken(tokenAccess);
+	next();		
+	} catch(e) {
+		console.log(e.message);
+		res.json(ApiError.forbidden("Not authorization"));
+		
+	}
+	
+	
+	
+}
